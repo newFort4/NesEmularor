0993a813b89dbc8f749d44840a11a3698888a8ad
davo <davydon@me.com>
2022-03-13 02:12:57 +0100

    Added Set/Clear OpCodes

---
+++ NesEmulator.Core.Tests/CPUTests/SmallCommandsTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace NesEmulator.Core.Tests.CPUTests
+{
+    public class SmallCommandsTests : CPUBaseTests
+    {
+        [Theory]
+        [InlineData(OpCodes.SED, SRFlags.Decimal)]
+        [InlineData(OpCodes.SEC, SRFlags.Carry)]
+        [InlineData(OpCodes.SEI, SRFlags.Interrupt)]
+        public void SetsFlagCorrectly(byte opCode, SRFlags flag)
+        {
+            cpu.Interpret(new byte[] { opCode, StopCode });
+
+            Assert.Equal(cpu.Status & ((byte)flag), (byte)flag);
+        }
+
+        [Theory]
+        [InlineData(OpCodes.CLD, SRFlags.Decimal)]
+        [InlineData(OpCodes.CLV, SRFlags.VOverflow)]
+        [InlineData(OpCodes.CLC, SRFlags.Carry)]
+        [InlineData(OpCodes.CLI, SRFlags.Interrupt)]
+        public void CearsFlagCorrectly(byte opCode, SRFlags flag)
+        {
+            cpu.Interpret(new byte[] { opCode, StopCode });
+
+            Assert.Equal(cpu.Status | (~(byte)flag), ~(byte)flag);
+        }
+    }
+}
