a6ab3fb759af010b2f01a28d4292c1fb3fb62bd6
newFort4 <davydon@me.com>
2022-03-31 14:40:38 +0200

    Added rom tetst

---
+++ NesEmulator.Core.Tests/ROMTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace NesEmulator.Core.Tests
+{
+    public class ROMTests
+	{
+		private const ushort PRGROMPageSize = 16384;
+		private const ushort CHRROMPageSize = 8192;
+
+		private record TestROM(byte[] Header, byte[] Trainer, byte[] PRGROM, byte[] CHRROM);
+
+		private byte[] CreateROM(TestROM testROM) => testROM
+				.Header
+				.Concat(testROM.Trainer ?? Array.Empty<byte>())
+				.Concat(testROM.PRGROM)
+				.Concat(testROM.CHRROM)
+				.ToArray();
+
+		[Fact]
+		public void ROMLoadsSuccessfully()
+        {
+			var prgROM = Enumerable.Repeat((byte)1, 2 * PRGROMPageSize).ToArray();
+			var chrROM = Enumerable.Repeat((byte)2, 1 * CHRROMPageSize).ToArray();
+
+			var testROM = CreateROM(new TestROM(
+				new byte[] { 0x4E, 0x45, 0x53, 0x1A, 0x02, 0x01, 0x31, 00, 00, 00, 00, 00, 00, 00, 00, 00 },
+				null,
+                prgROM,
+				chrROM
+			));
+
+            var rom = new ROM(testROM);
+
+			Assert.Equal(rom.CHRROM, chrROM);
+			Assert.Equal(rom.PRGROM, prgROM);
+			Assert.Equal(3, rom.Mapper);
+			Assert.Equal(Mirroring.Vertical, rom.Mirroring);
+		}
+
+		[Fact]
+		public void ROMLoadsSuccessfullyWithTrainer()
+        {
+			var prgROM = Enumerable.Repeat((byte)1, 2 * PRGROMPageSize).ToArray();
+			var chrROM = Enumerable.Repeat((byte)2, 1 * CHRROMPageSize).ToArray();
+
+			var testROM = CreateROM(new TestROM(
+				new byte[] {
+					0x4E,
+					0x45,
+					0x53,
+					0x1A,
+					0x02,
+					0x01,
+					0x31 | 0b100,
+					00,
+					00,
+					00,
+					00,
+					00,
+					00,
+					00,
+					00,
+					00,
+				},
+				Enumerable.Repeat((byte)0, 512).ToArray(),
+				prgROM,
+				chrROM
+			));
+
+			var rom = new ROM(testROM);
+
+			Assert.Equal(rom.CHRROM, chrROM);
+			Assert.Equal(rom.PRGROM, prgROM);
+			Assert.Equal(3, rom.Mapper);
+			Assert.Equal(Mirroring.Vertical, rom.Mirroring);
+		}
+	}
+}
+
---
+++ NesEmulator.Example/CPUExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using NesEmulator.Core;
+
+namespace NesEmulator.Example
+{
+    public static class CPUExtensions
+	{
+		public static string Trace(this CPU cpu)
+        {
+            return $"{cpu.ProgramCounter:X4}\t" +
+                $"{cpu.ReadMemory(cpu.ProgramCounter):X4}\t" +
+                $"{OpCode.Codes.FirstOrDefault(x => x.Code == cpu.ReadMemory(cpu.ProgramCounter)).Mnemonic}" +
+                $"A:{cpu.RegisterA:X4} X:{cpu.RegisterX:X4} Y:{cpu.RegisterY:X4} P:{cpu.Status:X4} SP:{cpu.StackPointer:X4}";
+        }
+	}
+}
+
